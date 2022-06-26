61f5c4a64c2b01c08dac90ab17e60ecd36455c0a
Juan <wlu922@yahoo.com>
2022-06-26 16:38:56 -0500

    Changed main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def main(msg):
+    print(msg)
 
-print("Hello World")
+main("Hello People")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
